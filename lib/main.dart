dd98623078197549e9984e8985d7bcf0f06fe0c1
prostreet1212@gmail.com <ktndlf,tcfyu2020>
2022-08-19 20:40:35 +0300

    версия 1

--- lib/main.dart
+++ lib/main.dart
@@ -59,33 +59,6 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   ];
 
-  Map<String, dynamic> data = {
-    'Мои фото': [
-      'https://picsum.photos/1200/501',
-      'https://picsum.photos/1200/502',
-      'https://picsum.photos/1200/503',
-      'https://picsum.photos/1200/504',
-      'https://picsum.photos/1200/505',
-      'https://picsum.photos/1200/506',
-      'https://picsum.photos/1200/507',
-      'https://picsum.photos/1200/508',
-      'https://picsum.photos/1200/509',
-      'https://picsum.photos/1200/510',
-    ],
-    'Галерея': [
-      'https://picsum.photos/1200/511',
-      'https://picsum.photos/1200/512',
-      'https://picsum.photos/1200/513',
-      'https://picsum.photos/1200/514',
-      'https://picsum.photos/1200/515',
-      'https://picsum.photos/1200/516',
-      'https://picsum.photos/1200/517',
-      'https://picsum.photos/1200/518',
-      'https://picsum.photos/1200/519',
-      'https://picsum.photos/1200/520',
-    ]
-  };
-
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -96,7 +69,6 @@ class _MyHomePageState extends State<MyHomePage> {
             centerTitle: true,
             bottom: TabBar(
                 tabs:
-                    //nav.map((String e) => Tab(text:e)).toList(),
                     pictureList.map((e) => Tab(text: e.keys.first,)).toList(),
                 indicator: BoxDecoration(color: Colors.lightBlue),),
           ),
@@ -122,15 +94,4 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-/*TabBarView(
-children: nav.map((name) {
-return ListView(
-key: PageStorageKey(name),
-children: <Widget>[
-...fakeData.map((e) {
-return Text(e);
-}).toList()
-],
-);
-}).toList(),
-),*/
+
